43bd34dd8d33eb73b4f6b21a0abe1d1c71c8952f
Gu2nSh2n <359525701@qq.com>
2017-08-01 19:09:02 +0800

    sa

--- src/main/java/com/atguigu/git/GitHubClass.java
+++ src/main/java/com/atguigu/git/GitHubClass.java
@@ -3,4 +3,13 @@ package com.atguigu.git;
 
 public class GitHubClass {
 
+    public static void main(String[] args) {
+        
+        
+        System.out.println("=========================================");
+        System.out.println("=========================================");
+        System.out.println("=========================================");
+        System.out.println("=========================================");
+        System.out.println("=========================================");
+    }
 }
